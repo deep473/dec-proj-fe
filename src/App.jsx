3db48b0671dbe48860bb1e6b9567f43975566cce
deep473 <deepztanshu@gmail.com>
2025-06-13 13:17:04 +0530

    13th june

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Sign_up from "./pages/Sign_up";
 import Sign_in from "./pages/Sign_in";
 import Admin_home from "./pages/Admin_home";
 import Customer_home from "./pages/Customer_home";
+import Add_product from "./pages/Add_product";
+import Show_products from "./pages/Show_products";
 
 function App() {
   
@@ -15,6 +17,10 @@ function App() {
           <Route path = "/sign_in"  element = {<Sign_in />} />
           <Route path = "/admin_home"  element = {<Admin_home />} />
           <Route path = "/customer_home"  element = {<Customer_home />} />
+
+          <Route path = "/addProd"  element = {<Add_product
+           />} />
+          <Route path = "/viewAllProd"  element = {<Show_products />} />
       </Routes>
     </>
   )
---
+++ src/pages/Show_products.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Show_products() {
+  return (
+    <div>Show_products</div>
+  )
+}
