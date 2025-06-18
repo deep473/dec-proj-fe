6e498682abc49ca67349bb53b6a0880da6a018f0
deep473 <deepztanshu@gmail.com>
2025-06-18 17:44:51 +0530

    cart1

--- src/pages/Customer_home.jsx
+++ src/pages/Customer_home.jsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Customer_home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
+  const fetchAllProducts = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/getAllProducts");
+      if (!res.ok) throw new Error("Failed to fetch products");
+      const data = await res.json();
+      setProducts(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) return <p>Loading products...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+
+  async function handleCart(productId) {
+    const username = localStorage.getItem("username")
+    
+    const data = { username, productId };
+    try {
+      const resp = await fetch("http://localhost:8080/addToCart", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "text/plain" 
+      },
+      body: JSON.stringify(data),
+    });
+      const msg = await resp.text(); 
+      if (msg === "success") {
+        alert("Add to cart:" + msg); 
+        navigate('/customer_home');
+      } 
+    } catch (err) {
+      console.error("Cart error:", err);
+      alert("Could not add to cart");
+    }
+  }
+
   return (
-    <div>Customer_home</div>
-  )
+    <>
+    <h3>Welcome, {localStorage.getItem("username")}</h3>
+
+    <table border="1" cellPadding="10" cellSpacing="0">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Photo</th>
+          <th>Category</th>
+          <th>More operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td>{product.id}</td>
+            <td>{product.name}</td>
+            <td>{product.description}</td>
+            <td>₹{product.price}</td>
+            <td>
+              <img src={product.photo} alt={product.name} width="100" height="80" />
+            </td>
+            <td>{product.category}</td>
+            <td>
+              <button 
+                    onClick={() => handleCart(product.id)}
+              >Add to cart</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </>
+  );
 }
